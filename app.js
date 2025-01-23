bd9782e018f2676710f030ac0690391b5d47914a
Aishah Perks <byaishah.altaf@gmail.com>
2025-01-23 10:40:13 +0000

    attempting th eweb storage workshop, however ran into a issue i can not figure out

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const colour = formData.get("colour");
+  localStorage.setItem("favColour", colour);
+});
